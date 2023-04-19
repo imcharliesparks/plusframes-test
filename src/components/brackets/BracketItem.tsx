39af4bbb16f4e1b7afe3cd50a7ac6d43a72ca00b
Charlie Sparks <sparksfullstack@gmail.com>
2023-04-19 18:11:25 -0400

    Charlie | Pulling sets and have the score now

--- src/components/brackets/BracketItem.tsx
+++ src/components/brackets/BracketItem.tsx
@@ -8,6 +8,8 @@ const BracketItem = ({ set }: BracketProps) => {
   const [playerOne, playerTwo] = set.slots
   const { name: playerOneName } = playerOne.entrant
   const { name: playerTwoName } = playerTwo.entrant
+  const { displayValue: playerOneScore } = playerOne.standing.stats.score
+  const { displayValue: playerTwoScore } = playerTwo.standing.stats.score
 
   return (
     <div className="rounded-[1rem] bg-white w-[80vw] my-3 py-6 drop-shadow-lg container">
@@ -18,7 +20,7 @@ const BracketItem = ({ set }: BracketProps) => {
           </span>
         </div>
         <div className="col-span-2">
-          vs
+          {playerOneScore} - {playerTwoScore}
         </div>
         <div className="col-span-4">
           <span className="font-normal text-sm">
